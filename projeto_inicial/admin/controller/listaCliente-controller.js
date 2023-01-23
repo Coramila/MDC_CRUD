415245494579488bfb50ba14569d67a71fadd079
Camila Carvalho <carvalho.camiilaa@gmail.com>
2023-01-23 17:44:36 -0300

    Async/Await

--- projeto_inicial/admin/controller/listaCliente-controller.js
+++ projeto_inicial/admin/controller/listaCliente-controller.js
@@ -19,21 +19,23 @@ const CriaNovaLinha = (nome, email, id) => {
 
 const tabela = document.querySelector("[data-tabela]");
 
-tabela.addEventListener("click", (evento) => {
+tabela.addEventListener("click", async (evento) => {
   let ehBotaoDeletar =
     evento.target.className === "botao-simples botao-simples--excluir";
 
   if (ehBotaoDeletar) {
     const linhaCliente = evento.target.closest("[data-id]");
     let id = linhaCliente.dataset.id;
-    clienteService.removeCliente(id).then(() => {
-      linhaCliente.remove();
-    });
+    await clienteService.removeCliente(id);
+    linhaCliente.remove();
   }
 });
 
-clienteService.listaClientes().then((data) => {
-  data.forEach((element) => {
+const render = async () => {
+  const listaClientes = await clienteService.listaClientes();
+  listaClientes.forEach((element) => {
     tabela.appendChild(CriaNovaLinha(element.nome, element.email, element.id));
   });
-});
+};
+
+render();
